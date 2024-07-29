52f410ff3de55650e0692f30a33242ab587c24fe
RavshanSean <ravshansean1995@gmail.com>
2024-07-29 18:01:53 -0400

    new stuff fixed

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import birdScene from '../assets/3d/bird.glb'
+import { useGLTF } from '@react-three/drei';
+
 const Bird = () => {
+const { scene, animation } = useGLTF(birdScene);
+
   return (
-    <mesh>
-      
+    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+     <primitive object={scene} />
     </mesh>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 
@@ -12,6 +12,8 @@ import Plane from '../models/Plane';
 </div> */}
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
     const adjustIslandForScreenSize = () => {
         let screenScale = null;
         let screenPosition = [0, -6.5, -43];
@@ -26,29 +28,49 @@ const Home = () => {
         return [screenScale, screenPosition, rotation];
     }
 
+    const adjustPlaneForScreenSize = () => {
+        let screenScale, screenPosition;
+
+        if(window.innerWidth < 768) {
+            screenScale = [1.5, 1.5, 1.5];
+            screenPosition = [0, -1.5, 0]
+        } else {
+            screenScale = [3, 3, 3];
+            screenPosition= [0, -4, -4]
+        }
+
+        return [screenScale, screenPosition];
+    }
+
     const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+    const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className='w-full h-screen relative'>
         <Canvas 
-         className='w-full h-screen bg-transparent'
+         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
          camera={{ near: 0.1, far: 1000 }}
          >
           <Suspense fallback={<Loader />}>
-            {/*its like a sun light from distance ply arond to get beter resulte*/}
              <directionalLight position={[1, 1, 1]} intensity={2} />
-             {/*this one its add sharp shodow dont do high its be ugly learn how to play with lights for next project*/}
              <ambientLight intensity={0.5}/>
-             {/*some lights no working findout why and fix leter */}
              <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1} />
+
              <Bird />
              <Sky />
              <Island 
                position={islandPosition} 
                scale={islandScale}
                rotation={islandRotation}
+               isRotating={isRotating}
+               setIsRotating={setIsRotating}
              />
-             <Plane />
+             <Plane
+               isRotating={isRotating}
+               planeScale={planeScale}
+               planePosition={planePosition}
+               rotation={[0, 20, 0]}
+               />
           </Suspense>
         </Canvas>
     </section>
